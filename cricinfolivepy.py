cbc43cda1d878a65f987a8ba89044cc57aa2c174
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-03-08 14:20:55 +0530

    added cricinfo

--- cricinfolivepy.py
+++ cricinfolivepy.py
@@ -2,6 +2,7 @@ from urllib.request import Request, urlopen
 import sys, time
 from bs4 import BeautifulSoup
 import re
+import pprint as pp
 
 root_url = "https://www.espncricinfo.com"
 
@@ -12,90 +13,83 @@ def sanitize(name):
 def get_batting_scores(batting_div):
     print()
     print('Batting:')
-    tables = batting_div.find_all("table", class_="table table-condensed")
-    for idt, table in enumerate(tables):
-        if idt==0:#Heading table
-            continue
-        rows = table.find_all("tr")
-        for idr, row in enumerate(rows):
-            if idr%2==0:
-                j = {}
-                cols = row.find_all("td")
-                name = cols[0].get_text()
-                # profile = cols[0].find_all("a")[0]['href']
-                r_b = re.split('\(|\)', cols[1].get_text())
-                fours = cols[2].get_text()
-                sixes = cols[3].get_text()
-                # s_r = cols[4].get_text()
-                j['name'] = sanitize(name)
-                # j['profile'] = profile
-                j['runs'] = int(r_b[0])
-                j['balls'] = int(r_b[1])
-                j['fours'] = int(fours)
-                j['sixes'] = int(sixes)
-                # j['s_r'] = 100*int(r_b[0])/int(r_b[1])
-                print(j)
+    rows = batting_div.find_all("div", class_="wrap batsmen")
+    for row in rows:
+        j = {}
+        cols = row.find_all("div", class_="cell")
+        name = cols[0].get_text()
+        runs = cols[2].get_text()
+        balls = cols[3].get_text()
+        fours = cols[4].get_text()
+        sixes = cols[5].get_text()
+        s_r = cols[6].get_text()
+        j['name'] = sanitize(name)
+        j['runs'] = int(runs)
+        j['balls'] = int(balls)
+        j['fours'] = int(fours)
+        j['sixes'] = int(sixes)
+        j['s_r'] = 100*int(runs)/int(balls)
+        print(j)
 
 def get_bowling_scores(bowling_div):
     print()
     print('Bowling:')
-    tables = bowling_div.find_all("table", class_="table table-condensed")
-    for idt, table in enumerate(tables):
-        rows = table.find_all("tr")
-        for idr, row in enumerate(rows):
-            if idr==0:#Bowler Subheading row
-                continue
-            j = {}
-            cols = row.find_all("td")
-            name = cols[0].get_text()
+    rows = bowling_div.find_all("tr", class_="")
+    for row in rows:
+        j = {}
+        cols = row.find_all("td", class_="")
+        if len(cols)<6:
+            continue
+        try:
+            name = cols[0].find_all("a")[0].get_text()
             overs = cols[1].get_text()
-            maiden = cols[2].get_text()
+            maidens = cols[2].get_text()
             runs = cols[3].get_text()
-            wicket = cols[4].get_text()
+            wickets = cols[4].get_text()
+            econ = cols[5].get_text()
             j['name'] = sanitize(name)
             j['overs'] = int(overs)
-            j['maiden'] = int(maiden)
+            j['maidens'] = float(maidens)
             j['runs'] = int(runs)
-            j['wicket'] = int(wicket)
+            j['wickets'] = float(wickets)
+            j['econ'] = float(econ)
             print(j)
+        except Exception as e:
+            print(e)
 
 def get_fow_scores(fow_div):
     print()
     print('Fall of Wickets:')
-    tables = fow_div.find_all("table", class_="table table-condensed")
-    for idt, table in enumerate(tables):
-        # if idt==0:#Heading table
-        #     continue
-        rows = table.find_all("tr")
-        for idr, row in enumerate(rows):
-            if idr==0:#Bowler Subheading row
-                continue
-            j = {}
-            cols = row.find_all("td")
-            wkt = cols[0].get_text()
-            runs = cols[1].get_text()
-            ovr = cols[2].get_text()
-            batsman = cols[3].get_text()
-            j['wkt'] = int(wkt)
-            j['runs'] = int(runs)
-            j['ovr'] = float(ovr)
-            j['batsman'] = batsman
-            print(j)
+    fows = fow_div.get_text().split('),')
+    for fow in fows:
+        print(fow)
 
 def get_match_scores(series_id, match_id, match_name):
     req = Request(root_url + '/series/' + series_id + "/scorecard/" + match_id + "/" + match_name, headers={'User-Agent': 'Mozilla/5.0'})
     html_source = urlopen(req).read()
     parsed_html = BeautifulSoup(html_source, 'html.parser')
 
-    batting_div = parsed_html.find_all("div", class_="scorecard-section batsmen")
-    bowling_div = parsed_html.find_all("div", class_="scorecard-section bowling")
+    batting_divs = parsed_html.find_all("div", class_="scorecard-section batsmen")
+    bowling_divs = parsed_html.find_all("div", class_="scorecard-section bowling")
+    fow_divs = parsed_html.find_all("div", class_="wrap dnb")
 
-    print(batting_div)
-    print(bowling_div)
-    # get_batting_scores(batting_div)
-    # get_bowling_scores(bowling_div)
-    # get_fow_scores(fow_div)
+    for inn in range(2):
+        print()
+        print("Innnings:", 1 + inn)
+        try:
+            get_batting_scores(batting_divs[inn])
+        except Exception as e:
+            print(e)
+
+        try:
+            get_bowling_scores(bowling_divs[inn])
+        except Exception as e:
+            print(e)
+
+        try:
+            get_fow_scores(fow_divs[inn])
+        except Exception as e:
+            print(e)
 
 get_match_scores(series_id = "19117", match_id = "1172164", match_name = "india-women-vs-england-women-2nd-t20i-eng-w-in-india-2018-19")
-
-
+get_match_scores(series_id = "19059", match_id = "1168243", match_name = "india-vs-australia-2nd-odi-aus-in-ind-2018-19")
